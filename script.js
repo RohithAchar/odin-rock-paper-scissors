8df747ac2438b6682d3b1d8b7e1d77d7d46302ce
Rohith <rohithachar2000@gamil.com>
2023-11-19 11:19:46 +0530

    scripting completed without style

--- script.js
+++ script.js
@@ -1,67 +1,93 @@
-//ALGORITHM
-//init playerScore to 0.
-//init computerScore to 0.
-//init array of [rock,paper,scissors]
-//init count to 0.
-//function getComputerChoice return random element from array of [rock,paper,scissors]
-//loop till count < 5.
-    //get input from computer.
-    //get input from user and convert it to lowercase.
-    //if player won add 10 to playerScore.
-    //if computer won add 10 to computerScore.
-    //if draw do nothing.
-//End loop
-//check who won.
+const title = document.querySelector('#title');
+const playerScoreEle = document.querySelector('#your-score');
+const computerScoreEle = document.querySelector('#computer-score');
+const playerChoiceEle = document.querySelector('#your-choice');
+const computerChoiceEle = document.querySelector('#computer-choice');
+const buttons = document.querySelectorAll('button');
 
-
+let playerChoice = "";
+let computerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
-const choices = ["rock","paper","scissors"];
+let round = 0;
 
-let count = 0;
-//function getComputerChoice return random element from array of [rock,paper,scissors]
-function getComputerChoice(){
-    const randomIndex = Math.floor(Math.random() * 3);
+buttons.forEach(btn => {
+    btn.addEventListener('click',playRound);
+});
+
+function reset(){
+    title.textContent = "Select your choice."
+    playerScoreEle.textContent = "";
+    computerScoreEle.textContent = "";
+    playerChoiceEle.textContent = "";
+    computerChoiceEle.textContent = "";
+    playerChoice = "";
+    computerChoice = "";
+    playerScore = 0
+    computerScore = 0
+    round = 0;
+}
+function randomComputerChoice(){
+    const choices = ['rock','paper','scissors'];
+    let randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
-//loop till count < 5.
-while(count < 5){
-    //get input from computer.
-    const computerChoice = getComputerChoice();
-    //get input from user and convert it to lowercase.
-    const playerChoice = prompt("Enter your choice").toLowerCase();
-
-    //if player won add 10 to playerScore.
-    //if computer won add 10 to computerScore.
-    //if draw do nothing.
+function addScore(playerChoice,computerChoice){
     if(playerChoice == computerChoice){
     }
-    
     else if(computerChoice == "rock" && playerChoice == "paper" ||
             computerChoice == "paper" && playerChoice == "scissors" ||
             computerChoice == "scissors" && playerChoice == "rock"
         ){
-            playerScore += 10;
+            playerScore += 1;
     }
     else{
-    computerScore += 10;
+        computerScore += 1;
     }
-    count++;
 }
-//End loop
-//check who won.
-if(playerScore === computerScore){
-    alert(`It's a draw.\n
-           Computer score = ${computerScore}\n
-           Player score = ${playerScore}`);
+function displayWinner(){
+    if(playerScore == computerScore){
+        title.textContent = "It's a draw";
+    }
+    else if(playerScore > computerScore){
+        title.textContent = "You won";
+    }
+    else{
+        title.textContent = "You lose";
+    }
 }
-else if(playerScore > computerScore){
-    alert(`You won.\n
-           Computer score = ${computerScore}\n
-           Player score = ${playerScore}`);
+function displayScore(){
+    playerScoreEle.textContent = "Your score : "+playerScore;
+    computerScoreEle.textContent = "Computer score : "+computerScore;
 }
-else{
-    alert(`You lost.\n
-           Computer score = ${computerScore}\n
-           Player score = ${playerScore}`);
+function displayChoice(playerChoice,computerChoice){
+    playerChoiceEle.textContent = "Your choice : "+playerChoice;
+    computerChoiceEle.textContent = "Computer choice : "+computerChoice;
+}
+function disableButtons(){
+    buttons.forEach(btn => {
+        btn.disabled = true;
+    });
+}
+function enableButtons(){
+    buttons.forEach(btn => {
+        btn.disabled = false;
+    });
+}
+function playRound(pChoice){
+    round++;
+    title.textContent = "Round : "+round;
+    playerChoice = pChoice.target.id;
+    computerChoice = randomComputerChoice();
+    displayChoice(playerChoice,computerChoice);
+    addScore(playerChoice,computerChoice);
+    displayScore();
+    if(playerScore == 5 || computerScore == 5){
+        displayWinner();
+        disableButtons();
+        setTimeout(function() {
+            reset();
+            enableButtons();
+        }, 2000);
+    }
 }
